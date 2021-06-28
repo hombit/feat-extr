7c8f9ba433569483fb4e0f8b0f3e4ee5e3d68add
Konstantin Malanchev <kostya@illinois.edu>
2021-06-28 01:52:33 -0500

    Add gal_b constrains into run.sh

--- run.sh
+++ run.sh
@@ -4,19 +4,18 @@ HOST=$1
 DIR=$2
 MINNOBS=$3
 
-NAME="short_gr_${MINNOBS}"
+NAME="extragal_short_gr_${MINNOBS}"
 SUFFIX="_${NAME}"
 
 RUSTFLAGS="-Ctarget-cpu=native" cargo run --release -- \
     clickhouse \
     "SELECT sid, mjd, filter, mag, magerr
       FROM ztf.dr4_source_obs_02
-      WHERE sid IN (SELECT sid
+      WHERE mjd <= 58664.0 AND sid IN (SELECT sid
         FROM ztf.dr4_source_meta_short_02
-        WHERE nobs_g >= ${MINNOBS} AND nobs_r >= ${MINNOBS}
+        WHERE nobs_g >= ${MINNOBS} AND nobs_r >= ${MINNOBS} AND abs(asin(sin(0.4734773249532946) * sin(pi() / 180. * dec) + cos(0.4734773249532946) * cos(pi() / 180. * dec) * cos(pi() / 180. * ra - 3.366032882941064)) * 180. / pi()) > 15.
       )
       ORDER BY sid, mjd
-
 " \
     --dir=${DIR} \
     --suffix=${SUFFIX} \
